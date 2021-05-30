48798e66f5ad696b7a2079f1989e102e6ad1dbf9
Valery Zinchenko <minicablestone@gmail.com>
2021-05-30 22:37:34 +0300

    add method

--- mandarin/src/LINKS.ts
+++ mandarin/src/LINKS.ts
@@ -8,8 +8,8 @@ import { GetUsersRequest } from "./interfaces"
 namespace Links {
   const fetchHost = "http://localhost"
   const fetchConfig: RequestInit = {}
-  async function request(endpoint: string, body?: any) {
-    const config = { ...fetchConfig, body }
+  async function request(method: "GET" | "POST", endpoint: string, body?: any) {
+    const config = { ...fetchConfig, method, body }
     const response = await fetch(fetchHost + endpoint, config)
     const payload = await response.json()
 
@@ -24,15 +24,15 @@ namespace Links {
   }
 
   export async function logIn(name: "admin" | String, password: "admin" | String) {
-    const payload = await request("/auth", {
+    const payload = await request("POST", "/auth", {
       name, password
     })
 
     return handleError(payload)
   }
 
-  export async function addUser(name: any, password: any, role: any, warehouse_id: number) {
-    const payload = await request("/addUser", {
+  export async function addUser(name: any, password: any, role: any, warehouse_id: any) {
+    const payload = await request("POST", "/addUser", {
       name, password, role, warehouse_id
     })
 
@@ -40,7 +40,7 @@ namespace Links {
   }
 
   export async function removeUser(user_id: number) {
-    const payload = await request("/removeUser", {
+    const payload = await request("POST", "/removeUser", {
       user_id
     })
 
@@ -48,14 +48,14 @@ namespace Links {
   }
 
   export async function getUsers(): Promise<GetUsersRequest> {
-    const payload = await request("/users")
+    const payload = await request("GET", "/users")
 
     return payload
   }
 }
 
-export function usePromise<T = any>(promise: Promise<T>): T {
-  const [result, setResult] = useState(null)
+export function usePromise<T = any>(promise: Promise<T>) {
+  const [result, setResult] = useState<T | null>(null)
 
   useEffect(() => {
     promise.then(setResult)
