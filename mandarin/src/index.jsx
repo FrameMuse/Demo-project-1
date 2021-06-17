9724530aaede91c801f0089ca986afe9c6b58a1b
Valery Zinchenko <minicablestone@gmail.com>
2021-06-17 20:52:23 +0300

    Update 6

--- mandarin/src/index.jsx
+++ mandarin/src/index.jsx
@@ -1,7 +1,15 @@
+import { createContext } from "react"
 import ReactDom from "react-dom"
 import App from "./App"
+import Links from "./LINKS"
 
-ReactDom.render(
-  <App />,
-  document.getElementById("root")
-)
+export const UserContext = createContext()
+const userRequest = Links.getUserInfo()
+userRequest.then(user => {
+  ReactDom.render(
+    <UserContext.Provider value={user}>
+      <App />
+    </UserContext.Provider>,
+    document.getElementById("root")
+  )
+})
